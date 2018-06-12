4dd4d8582c67cc913605833eac54e866e1895ae0
AsherLMac <renpsybeta@gmail.com>
2018-06-12 13:38:27 -0400

    Ls & Pwd

--- bash.js
+++ bash.js
@@ -1,12 +1,16 @@
-process.stdout.write('prompt >');
+const exportedPwd = require('./pwd');
+const exportedLs = require('./ls');
 
+process.stdout.write('prompt >');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
 
     if (cmd === 'pwd1') {
-        process.stdout.write(process.cwd());
+        exportedPWD.pwd1();
+    } else if (cmd === 'ls1') {
+        exportedLs.ls();
     } else {
-        process.stdout.write('You Write: ' + cmd);
+        process.stdout.write('You Wrote: ' + cmd);
         process.stdout.write('\nprompt >');
     }
 })
